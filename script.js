74496cafda3ca8e8554396c81c21db051628456e
omoalfa <engr.omoalfa@gmail.com>
2020-08-27 12:04:04 +0100

    complete light mode style, add script file

---
+++ script.js
@@ -0,0 +1,20 @@
+const h2 = document.getElementsByTagName("h2");
+const h3 = document.getElementsByTagName("h3");
+const value = document.querySelectorAll(".value");
+const dvalue = document.querySelectorAll(".dvalue");
+const body = document.querySelector("body");
+const header = document.querySelector("header");
+const toggler = document.getElementById("toggle-button");
+const ball = document.getElementById("ball")
+
+function toggle () {
+    console.log("toggle")
+
+// h2 and h3 changes white
+// .value and .dvalue changes white
+// toggle botton changes dark
+// body background changes dark
+// header background changes darker
+// cards background changes darkest
+}
+
